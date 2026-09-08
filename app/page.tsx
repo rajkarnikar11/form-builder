b12fd7f238e7a8ad7d38c961c26a3a56dca9ee82
salil <salil.rajkarnikar@insyde.ai>
2026-09-08 15:59:05 +0545

    refactro

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className=" h-full " >
       <div className="p-6 sticky top-0 z-100 bg-teal-900 text-gray-50 "><h1 className=" font-bold text-4xl"> Form Builder</h1></div>
-      <div className="p-6 flex h-full gap-4">
+      <div className="p-6 flex h-full gap-4 overflow-x-hidden">
         <Formstructure />
         <LiveForm />
       </div>
